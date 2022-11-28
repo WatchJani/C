a9a6244a754d458093fb8b73243078ada14dad8c
Janko <45310863+WatchJani@users.noreply.github.com>
2022-11-28 11:56:33 +0100

    Add files via upload

---
+++ C/helloJanko/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+
+int main(){
+
+    // int my_integer = 10;
+    // long my_long = 100000l;
+    // printf("My integer: %i\n", my_integer);
+    // printf("My long: %li\n", my_long);
+
+    // float my_float = 3.5f;
+    // printf("My float: %f\n", my_float);
+
+    // double my_double = 3.5;
+    // printf("My double: %lf\n", my_double);
+
+    // char my_character = 65;
+    // printf("My character: %c\n", my_character);
+
+    if(true){
+        printf("asdasd\n");
+    }
+
+}
